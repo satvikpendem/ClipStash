23dc6c5d29ba7770f2106cefae95831bea6c779e
Satvik Pendem <satvik.pendem@gmail.com>
2022-08-26 22:09:30 -0400

    Begin sqlx integration

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 pub mod lib {
-    pub mod data;
+    pub mod data {
+        pub mod model;
+    }
     pub mod domain {
         pub mod clip;
         pub mod time;
@@ -7,3 +9,8 @@ pub mod lib {
     pub mod service;
     pub mod web;
 }
+
+pub use lib::domain::{
+    clip::{field::shortcode::ShortCode, ClipError},
+    time::Time,
+};
--- src/lib/data.rs
+++ src/lib/data.rs
@@ -2,7 +2,50 @@ use std::str::FromStr;
 
 use derive_more::{Display, From};
 use serde::{Deserialize, Serialize};
+use sqlx::{
+    sqlite::{SqlitePool, SqlitePoolOptions, SqliteQueryResult, SqliteRow},
+    Pool, Sqlite,
+};
 use uuid::Uuid;
+
+#[derive(Debug, thiserror::Error)]
+#[allow(clippy::module_name_repetitions)]
+pub enum DataError {
+    #[error("Database error: {0}")]
+    Database(#[from] sqlx::Error),
+}
+
+pub type AppDatabase = Database<Sqlite>;
+pub type DatabasePool = SqlitePool;
+pub type Transaction<'t> = sqlx::Transaction<'t, Sqlite>;
+pub type AppDatabaseRow = SqliteRow;
+pub type AppQueryResult = SqliteQueryResult;
+
+pub struct Database<D: sqlx::Database>(Pool<D>);
+
+impl Database<Sqlite> {
+    /// # Panics
+    ///
+    /// This function panics if the database cannot be connected to.
+    pub async fn new(connection_str: &str) -> Self {
+        let pool = SqlitePoolOptions::new().connect(connection_str).await;
+
+        match pool {
+            Ok(pool) => Self(pool),
+            Err(err) => panic!(
+                "Failed to connect to database: {}\n\
+                \nIf the database has not yet been created, run:\n\
+                \t$ sqlx database setup\n",
+                err
+            ),
+        }
+    }
+
+    #[must_use]
+    pub fn get_pool(&self) -> &DatabasePool {
+        &self.0
+    }
+}
 
 #[derive(Debug, Clone, Serialize, Deserialize, Default, From, Display)]
 pub struct DbId(Uuid);
